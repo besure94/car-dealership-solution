66aa8b059c4750d16f89fc3ee66bc88fc81a8561
Brian Scherner <brianscherner@gmail.com>
2023-10-03 09:55:15 -0700

    Add routes for Index and CreateForm + form to list new cars.

--- CarDealership/Controllers/HomeController.cs
+++ CarDealership/Controllers/HomeController.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using CarDealership.Models;
 
 namespace CarDealership.Controllers
 {
   public class HomeController : Controller
   {
+    [Route("/")]
+    public ActionResult Index()
+    {
+      Car starterCar = new Car(1970, "Chevrolet", "Impala");
+      return View(starterCar);
+    }
 
+    [Route("/cars/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
 
   }
 }
